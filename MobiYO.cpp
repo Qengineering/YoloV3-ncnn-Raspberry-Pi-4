998f72f027913f197b36ace3293931d097d36bca
Q-engineering <info@qengineering.eu>
2019-09-11 15:45:59 +0200

    C++ example file

--- MobiYO.cpp
+++ MobiYO.cpp
@@ -16,6 +16,7 @@
 #include <vector>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 #include "platform.h"
